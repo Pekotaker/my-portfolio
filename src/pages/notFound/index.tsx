1cdf4167ea9418642b5cbe58df00d93883da2cd7
Pekotaker <loliisdabezt@gmail.com>
2024-06-18 18:24:34 +0700

    add a go home button

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -5,6 +5,9 @@ const NotFound: React.FC = () => {
     <div>
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
+      <button>
+        <a href="/">Go Home</a>
+      </button>
     </div>
   );
 };
