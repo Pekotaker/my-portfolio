6166182e3617b9ecaae6b4f3330349ced77dbc58
Pekotaker <loliisdabezt@gmail.com>
2024-06-18 18:33:25 +0700

    edit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome to My Portfolio</h1>
-      <p>This is the welcome page of my portfolio website.</p>
+      {/* <p>This is the welcome page of my portfolio website.</p> */}
     </div>
   );
 };
